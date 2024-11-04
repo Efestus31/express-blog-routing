22221e8991213910cdb4a709cf1708a77f82b162
Efestus31 <biagiohd@gmail.com>
2024-11-04 16:08:30 +0100

    add  parametri dinamici per stabilire i path

--- controller/postController.js
+++ controller/postController.js
@@ -1,11 +1,32 @@
 const { json } = require('express')
 const posts = require('../db/db.js')
 
-const index = (req, res) =>  {
+const index = (req, res) => {
 
     //send response with the 200 status
-    res.status(200).json({
-        data: posts,
-        counter: posts.length
-    })
+    res.status(200).json(
+        {
+            data: posts,
+            counter: posts.length
+        }
+    )
+}
+
+const show = (req, res) => {
+     const post = posts.find(post => post.slug === req.params.slug)
+
+     console.log(post);
+     if (!post){
+        return res.status(404).json({
+            error: `404! Not found`
+        })
+     }
+     return res.json({
+        data:post
+     })
+}
+
+module.exports = {
+    index,
+    show
 }
