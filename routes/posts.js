8cd97e4cd762cd8beed3cfb8d426fac0866f0da7
Efestus31 <biagiohd@gmail.com>
2024-11-04 16:16:33 +0100

    add index and show routes add export

---
+++ routes/posts.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+//define the router instance
+const router = express.Router()
+const postController = require('../controller/postController.js')
+
+//define all routes here
+
+//Post API
+router.get('/', postController.index)
+
+//show only the single(selected resources)
+router.get('/:slug', postController.show)
+
+//export the router instance
