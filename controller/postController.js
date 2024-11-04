637eb2b75740eda9311c8924105ec2aea2d78842
Efestus31 <biagiohd@gmail.com>
2024-11-04 15:25:11 +0100

    add funzione index

--- controller/postController.js
+++ controller/postController.js
@@ -1,2 +1,11 @@
 const { json } = require('express')
 const posts = require('../db/db.js')
+
+const index = (req, res) =>  {
+
+    //send response with the 200 status
+    res.status(200).json({
+        data: posts,
+        counter: posts.length
+    })
+}
