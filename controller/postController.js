2b8a4188aa8b72af88338bc86431f86992378a93
Efestus31 <biagiohd@gmail.com>
2024-11-04 17:51:25 +0100

    debug bonus

--- controller/postController.js
+++ controller/postController.js
@@ -7,6 +7,8 @@ const fs = require('fs')
 
 const index = (req, res) => {
 
+    console.log(posts);
+    
     //send response with the 200 status
     res.status(200).json(
         {
@@ -33,19 +35,28 @@ const show = (req, res) => {
 const store = (req, res) =>{
     //Crea un nuovo oggetto all'interno del db
     const post ={
-        title: req.body.name,
+        title: req.body.title,
         slug: req.body.slug,
         content: req.body.content,
         image: req.body.image,
         tags: req.body.tags,
     }
+
+    //validazione dei campi dell'oggetto
+    if (!post.title || !post.slug || !post.content) {
+        return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    //aggiunge il nuovo post all'array
+    posts.push(post)
+
     //aggiorna file db
     fs.writeFileSync('./db/db.js',`module.exports = ${JSON.stringify(posts, null, 4)}`)
 
     res.json({
         data: posts,
         status: 201,
-        counter: posts.lenght
+        counter: posts.length
     })
 }
 
