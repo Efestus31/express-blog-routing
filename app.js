ae23b8eeedb6a587ee996812ead7e12a5189535f
Efestus31 <biagiohd@gmail.com>
2024-11-04 16:32:39 +0100

    add routes to app.js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+const postRouter = require('./routes/posts.js')
 
 
 const host = process.env.HOST
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
-    res.send('<h1> Benvenuto nel mio blog! </h1>')
+    res.send(' Benvenuto nel mio blog!')
 })
 
 app.listen(port, (req, res) => {
     console.log(`Server is running at ${host}:${port}`);
 })
+
+app.use('/posts', postRouter)
+
+app.use('/:slug', postRouter)
--- routes/posts.js
+++ routes/posts.js
@@ -7,9 +7,10 @@ const postController = require('../controller/postController.js')
 //define all routes here
 
 //Post API
-router.get('/', postController.index)
+router.get('/posts', postController.index)
 
 //show only the single(selected resources)
 router.get('/:slug', postController.show)
 
 //export the router instance
+module.exports = router
